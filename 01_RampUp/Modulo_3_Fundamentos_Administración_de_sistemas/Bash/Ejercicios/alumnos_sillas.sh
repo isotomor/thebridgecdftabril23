58c38ccee6e0efbca53edf1a337e7ed04a0026a9
isotomor <i.sotomoreno@gmail.com>
2023-04-26 09:32:50 +0200

    Actualizamos ...

--- 01_RampUp/Modulo_3_Fundamentos_Administración_de_sistemas/Bash/Ejercicios/alumnos_sillas.sh
+++ 01_RampUp/Modulo_3_Fundamentos_Administración_de_sistemas/Bash/Ejercicios/alumnos_sillas.sh
@@ -1,6 +1,3 @@
-#! /bin/bash
-
-#declare -a usuarios=([0]=num1 [1]=num2 [2]=num3 [3]=num4)
 #!/bin/bash
 
 # Pedimos al usuario que introduzca los parámetros separados por espacios
@@ -12,33 +9,31 @@ read cantidad_sillas
 
 if (( $cantidad_alumn > $cantidad_sillas )); then
 
-     echo  "No hay sufientes sillas para los alumnos"
+  echo  "No hay sufientes sillas para los alumnos"
+
 else
-        alumnos=()
-        sillas=()
-#En este caso el i=1 marca el inicio del bucle luego la condicion y luego q se valla sumando 1
+  alumnos=()
+  sillas=()
+  # En este caso el i=1 marca el inicio del bucle luego la condicion y luego q se valla sumando 1
 
-         for ((i=1; i<=$cantidad_alumn; i++)) 
-         do
- 
-#Pido el nombre del alumno 
+  for ((i=1; i<=$cantidad_alumn; i++))
+  do
+    #Pido el nombre del alumno
+    echo "Dame el nombre del alumno $i:"
+    read nombre_alumn
 
-             echo "Dame el nombre del alumno $i:"
-             read nombre_alumn
+    echo "Dame el numero de la silla de $nombre_alumn:"
+    read numero_silla
 
-             echo "Dame el numero de la silla de $nombre_alumn:"
-             read numero_silla
-  
-#Aqui con el alumnos+ lo que hacemos es tomar la variable leida y añadirla a alumnos
-          alumnos+=($nombre_alumn)
-          sillas+=($numero_silla)
-        done
- 
-# Asociar aleatoriamente los nombres de los alumnos con los números de silla
+    #Aqui con el alumnos+ lo que hacemos es tomar la variable leida y añadirla a alumnos
+    alumnos+=($nombre_alumn)
+    sillas+=($numero_silla)
+  done
 
-reparto=$(paste <(printf "%s\n" "${alumnos[@]}") <(printf "%s\n" "${sillas[@]}" | shuf))
+  # Asociar aleatoriamente los nombres de los alumnos con los números de silla
+  reparto=$(paste <(printf "%s\n" "${alumnos[@]}") <(printf "%s\n" "${sillas[@]}" | shuf))
 
-# Mostrar la asociación aleatoria
+  # Mostrar la asociación aleatoria
   echo "A continuacion os diremos donde se sienta al final cada uno:"
   echo "$reparto"
 fi
