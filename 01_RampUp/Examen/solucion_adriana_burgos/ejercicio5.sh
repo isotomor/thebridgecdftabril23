7f16853ac595f1ae0a2560ba2d65fa6f8304d59b
Adriana Burgos <adrianaburgosbra@gmail.com>
2023-04-27 19:07:35 +0200

    Ejercicio 5 actualizado

--- 01_RampUp/Examen/solucion_adriana_burgos/ejercicio5.sh
+++ 01_RampUp/Examen/solucion_adriana_burgos/ejercicio5.sh
@@ -12,12 +12,21 @@ res=0
 # Recibimos el numero del usuario
 echo "Dame un numero"
   read num
-i=num
+
+# Nuestro contador i se inicializa a num - 1 ya que el factorial 
+# se va multiplicando siempre por si mismo - 1. El resultado se inicializa a 1
+i=num-1
 res=num
 
 echo "vamos a caulcular el factorial de $num :"
+
+# Mientras nuestro contador i sea mayor que 0, multiplicamos el resultado
+# por i y le restamos 1 a i para seguir iterando. 
+# Tambien se podría hacer le bucle con al lógica inversa: mientras i sea menor que num, 
+# i+=1 para iterar.
 while [ $i -gt 0 ]; do
     res=res*i
     i=i-1
-done 
+done
 echo "el factorial de $num es $res"
+
