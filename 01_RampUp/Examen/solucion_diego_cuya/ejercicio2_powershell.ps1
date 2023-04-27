4bf10f56d13bbf6e63c69d3741c06cc957697063
DiegoCosaVelarde <crow262728@gmail.com>
2023-04-27 12:27:21 +0200

    Añado mas explicaciones

--- 01_RampUp/Examen/solucion_diego_cuya/ejercicio2_powershell.ps1
+++ 01_RampUp/Examen/solucion_diego_cuya/ejercicio2_powershell.ps1
@@ -1,5 +1,5 @@
 #Numeros aleatorios
-#Usamos la funcion Get-random para que nos de numero aleatorio.
+#Usamos la funcion Get-random para que nos de numero aleatorio y gracias a minimun y maximun podemos elegir entre que numeros queremso que salgan, lo probe sin eso y daba numero muy grandes.
 #Primero creamos el array numA para los numeros aleatorios
 $numA = @()
 
@@ -9,6 +9,7 @@ for ($i = 0; $i -lt 10; $i++){
 
 Write-Host "El array original es este: $numA"
 
+#Ahora he encontrado que la funcion "$numA".count -1 da a entender que debe empezar por el final del array, con -ge indicamos que si i es menor igual 0 siga, para que valla bajando hasta llegar al primer numero y el -- da a entender q se quite uno osea que se valla una posicion antes al array.
 for($i = $numA.Count - 1; $i -ge 0; $i--){
     Write-Host  $numA[$i]
 }
