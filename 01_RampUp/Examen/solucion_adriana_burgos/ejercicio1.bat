a369973d7d84ae1a92b9e3be9f581b7aa8e78d1a
Adriana Burgos <adrianaburgosbra@gmail.com>
2023-04-27 12:49:29 +0200

    Add ejercicio1.bat

---
+++ 01_RampUp/Examen/solucion_adriana_burgos/ejercicio1.bat
@@ -0,0 +1,4 @@
+:: 1. Tomar el fichero titanic.csv y crear un script que nos diga cuántos pasajeros sobrevivieron de los EMBARKED en 
+:: S (Southampton), C (Cherbourg), y Q (Queenstown). Asimismo, cuantos no aparece nada en ese campo.
+
+:: He dejado este ejercicio para el último y estoy saturada, así que no he podido sacarlo.
